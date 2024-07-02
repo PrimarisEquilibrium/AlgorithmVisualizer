ae596e6a49897077d6071cba288396652e1d0e8e
Kevin <incorrectclock1@gmail.com>
2024-07-02 19:30:08 -0400

    Basic Cell and CellArray classes created

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+from enum import Enum
+
 
 # pygame setup
 pygame.init()
@@ -10,14 +12,61 @@ font = pygame.font.Font("fonts/Roboto-Regular.ttf", 25)
 clock = pygame.time.Clock()
 
 PRIMARY_COLOR = "#151515"
-SECONDARY_COLOR = "#d3d3d3"
+SECONDARY_COLOR = "#a6a6a6"
+INACTIVE_COLOR = "#454545"
+SELECTED_COLOR = "#ffffff"
+
+
+# Enumeration containing all the states a cell could be in
+class CellState(Enum):
+    ACTIVE = 1
+    INACTIVE = 2
+    SELECTED = 3
+
+# A Cell class containing a value and visual state
+class Cell():
+    def __init__(self, value, state = CellState.ACTIVE):
+        self.value = value
+        self.state = state
+
+    def __str__(self):
+        return f"Cell({self.value}, {self.state.name})"
+
+    def set_active(self):
+        self.state = CellState.ACTIVE
+    
+    def set_inactive(self):
+        self.state = CellState.INACTIVE
+    
+    def set_selected(self):
+        self.state = CellState.SELECTED
+    
+    def get_color(self):
+        match self.state:
+            case CellState.ACTIVE:
+                return SECONDARY_COLOR
+            case CellState.INACTIVE:
+                return INACTIVE_COLOR
+            case _:
+                return SELECTED_COLOR
+
+# CellArray class with methods to operate on all cells
+class CellArray():
+    def __init__(self, *args):
+        self.array = self.make_cell_array(*args)
+
+    def __str__(self):
+        return "[" + ", ".join(str(cell) for cell in self.array) + "]"
+    
+    def make_cell_array(self, *args):
+        return [Cell(arg) for arg in args]
 
 
 def binary_search(arr, val):
     """
     Returns the index of a given value in a sorted array.
     
-    :param arr: Array to search.
+    :param arr: Cell array to search.
     :param val: The value to search for.
     :return: The index of the value or False if it was not found.
     """
@@ -42,40 +91,52 @@ def binary_search(arr, val):
     print("Result not found")
     return False
 
-print(binary_search([10, 20, 30, 40, 50], 50))
-
-
 # Array cell properties
 rect_size = 75
 border = 5
-def draw_array(arr, x_offset, y_offset):
+def draw_cell(cell, x, y):
     """
-    Draws a horizontal sequence of cells, where each cell corresponds to an element in the given array.
+    Draws a single cell with its label and visual state.
 
-    :param arr: Array to draw.
+    :param cell: Cell object.
+    :param x: X-Position of cell.
+    :param y: Y-Position of cell.
+    """
+
+    value = cell.value
+    color = cell.get_color()
+    
+    # Create Rect object
+    rect = pygame.Rect(x, y, rect_size, rect_size)
+    
+    # Draw cell and border
+    pygame.draw.rect(screen, PRIMARY_COLOR, rect)
+    pygame.draw.rect(screen, color, rect, border)
+
+    # Render text in cell
+    text = font.render(f"{value}", True, color)
+    padding = rect_size / 2
+    text_x = x + padding
+    text_y = y + padding
+    text_rect = text.get_rect(center=(text_x, text_y))
+    screen.blit(text, text_rect)
+
+def draw_cell_array(cell_arr, x_offset, y_offset):
+    """
+    Draws a horizontal sequence of cells, where each cell corresponds to an element in the given cell array.
+
+    :param cell_arr: Cell array to draw.
     :param x_offset: X-Offset from top left corner of screen.
     :param y_offset: Y-Offset from top left corner of screen.
     """
 
-    for i, display_text in enumerate(arr):
+    for i, cell in enumerate(cell_arr):
         # Position of cell
         x = x_offset + (i * rect_size)
         y = y_offset + 0
 
-        # Create Rect object
-        rect = pygame.Rect(x, y, rect_size, rect_size)
-        
-        # Draw cell and border
-        pygame.draw.rect(screen, PRIMARY_COLOR, rect)
-        pygame.draw.rect(screen, SECONDARY_COLOR, rect, border)
+        draw_cell(cell, x, y)
 
-        # Render text in cell
-        text = font.render(f"{display_text}", True, SECONDARY_COLOR)
-        padding = rect_size / 2
-        text_x = x + padding
-        text_y = y + padding
-        text_rect = text.get_rect(center=(text_x, text_y))
-        screen.blit(text, text_rect)
 
 def init():
     running = True
@@ -86,8 +147,6 @@ def init():
                 running = False
         
         screen.fill("#151515")
-
-        draw_array([0, 1, 2, 3, 4], 50, 50)
 
         # Displays contents onto screen
         pygame.display.update()
