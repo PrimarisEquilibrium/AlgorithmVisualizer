a7799fef67d28ebe9849f3b395a27f5a44f5a940
Kevin <incorrectclock1@gmail.com>
2024-07-05 14:58:08 -0400

    Added InputBox focus system

--- src/main.py
+++ src/main.py
@@ -35,11 +35,13 @@ class InputBox:
             manager=self.manager,
             font_color=colors.PRIMARY_COLOR, 
             cursor_color=colors.PRIMARY_COLOR, 
-            font_object=font
+            font_object=font,
+            cursor_width=0
         )
         self._text_box = pygame.Rect(x, y, width, height)
+        self.is_focused = False
     
-    def draw(self, events: list[pygame.event.Event]):
+    def draw(self, events: list[pygame.event.Event]) -> None:
         # Draw input box label
         text = font.render(f"{self.label}", True, colors.SELECTED_COLOR)
         screen.blit(text, (self.x, self.y - 35))
@@ -49,10 +51,29 @@ class InputBox:
         pygame.draw.rect(screen, colors.PRIMARY_COLOR, self._text_box, 5)
 
         # Draw the text input
-        self.textinput.update(events)
+        if (self.is_focused):
+            # Only update the text input if it is in focus
+            self.textinput.update(events)
         _, text_height = self.textinput.surface.get_size()
         padding = (self.height - text_height) // 2
         screen.blit(self.textinput.surface, (self.x + padding, self.y + padding))
+    
+    def cursor_in_textbox(self, mouse_x: int, mouse_y: int) -> None:
+        """ Returns true if the cursor is located inside the text_box rect """
+        if (mouse_x >= self.x and mouse_y >= self.y and
+            mouse_x <= self.x + self.width and mouse_y <= self.y + self.height):
+            return True
+        return False
+        
+    def toggle_focus(self) -> None:
+        # Toggle is_focused variable and cursor
+        if (not self.is_focused):
+            self.textinput.cursor_width = 4
+            self.is_focused = True
+        else:
+            self.textinput.cursor_width = 0
+            self.is_focused = False
+            
 
 input_box = InputBox("Array to search (seperated by commas)", 200, 200, 500, 50)
 val_box = InputBox("Value to find", 200, 300, 500, 50)
@@ -82,6 +103,12 @@ def init() -> None:
             # Allows the window to be closed on QUIT ("X" at top right of the window)
             if event.type == pygame.QUIT:
                 running = False
+            
+            # Handles input box focus
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                if (input_box.cursor_in_textbox(mouse_x, mouse_y)):
+                    input_box.toggle_focus()
 
             # Handle input text submission
             if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
