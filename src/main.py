924b4393cb2831efe3b930231fefe1c1a2246d82
Kevin <incorrectclock1@gmail.com>
2024-07-05 13:30:36 -0400

    Added basic text input and fixed flickering Text input box randomly flickered if placed higher in the game loop code

--- src/main.py
+++ src/main.py
@@ -20,7 +20,9 @@ clock = pygame.time.Clock()
 my_cell_array = CellArray(screen, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
 my_binary_search = BinarySearch(my_cell_array, 3)
 
-my_text_input = pygame_textinput.TextInputVisualizer()
+my_text_input = pygame_textinput.TextInputVisualizer(
+    font_color=colors.PRIMARY_COLOR, cursor_color=colors.PRIMARY_COLOR
+)
 
 def init() -> None:
     my_time = pygame.time.get_ticks()
@@ -28,23 +30,24 @@ def init() -> None:
     running = True
 
     while running:
-        events = pygame.event.get()
-
         screen.fill(colors.BACKGROUND_COLOR)
 
-        # Allows the window to be closed on QUIT ("X"  at top right of the window)
-        for event in events:
-            if event.type == pygame.QUIT:
-                running = False
-        
         my_binary_search.render()
-
         current_time = pygame.time.get_ticks()
         if current_time - my_time >= interval:
             screen.fill(colors.BACKGROUND_COLOR)
             my_time = current_time
             my_binary_search.render()
             my_binary_search.search_step()
+
+        events = pygame.event.get()
+        my_text_input.update(events)
+        screen.blit(my_text_input.surface, (50, 50))
+
+        # Allows the window to be closed on QUIT ("X"  at top right of the window)
+        for event in events:
+            if event.type == pygame.QUIT:
+                running = False
 
         # Displays contents onto screen
         pygame.display.update()
