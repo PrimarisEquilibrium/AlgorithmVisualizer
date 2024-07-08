57448cd6d5d0c784485f02852dbfb588be2c421a
Kevin <incorrectclock1@gmail.com>
2024-07-08 09:02:50 -0400

    Added a way to navigate between algorithms - To be further abstracted in the future

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import pygame
+from typing import Optional, Any
 
 from cell import CellArray
 from algorithms import BinarySearch
@@ -18,31 +19,6 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 
-    
-def cell_array_init(user_input):
-    try:
-        # String cleaning
-        user_array = ''.join(user_input.split())
-        user_array = user_array.split(",")
-        
-        # Converts each string number into an integer
-        user_array = list(map(lambda x : int(x), user_array))
-
-        cell_array = CellArray(screen, user_array)
-        return cell_array
-    except ValueError:
-        print("Invalid input")
-
-
-input_box = InputBox(screen, "Array to search (seperated by commas)", 35, 60, 500, 50, 30)
-val_box = InputBox(screen, "Value to find", 35, 160, 500, 50)
-
-def get_input_data():
-    cell_array = cell_array_init(input_box.get_value())
-    val = int(val_box.get_value())
-    return cell_array, val
-
-submit_btn = Button(screen, "Submit", 35, 240, 125, 60, get_input_data)
 
 class AlgorithmVisualizer:
     def __init__(self) -> None:
@@ -52,19 +28,62 @@ class AlgorithmVisualizer:
         self.running = True # Toggles if the game loop is running
         self.state = "home"
         self.algorithm_chosen = None
+
+        # Each algorithm may or may not have a different UI, therefore store them arbitrarily in a list
+        self.input_boxes = []
+        self.buttons = []
+
+        # home_buttons[X][0] is the label of the button
+        # home_buttons[X][1] is the button object
+        self.home_buttons = [
+            ("bsa", Button(screen, "Binary Search Algorithm", 50, 50, 260, 60, (lambda x: None)))
+        ]
+
+    def cell_array_init(self, user_input: str) -> Optional[CellArray]:
+        try:
+            # String cleaning
+            user_array = ''.join(user_input.split())
+            user_array = user_array.split(",")
+            
+            # Converts each string number into an integer
+            user_array = list(map(lambda x : int(x), user_array))
+
+            cell_array = CellArray(screen, user_array)
+            return cell_array
+        except ValueError:
+            print("Invalid input")
+
+    def get_input_data(self) -> list[Any]:
+        results = []
+        # Returns all the typed values in all input boxes
+        for input_box in self.input_boxes:
+            results.append(input_box.get_value())
+        return results
+
+    def initialize_ui_elements(self):
+        # Depending on what algorithm is chosen initialize different UI elements
+        if self.algorithm_chosen == "bsa":
+            self.input_boxes = [
+                InputBox(screen, "Array to search (seperated by commas)", 35, 60, 500, 50, 30),
+                InputBox(screen, "Value to find", 35, 160, 500, 50)
+            ]
+            self.buttons = [
+                Button(screen, "Submit", 35, 240, 125, 60, self.get_input_data)
+            ]
     
     def run(self) -> None:
         while self.running:
             screen.fill(colors.BACKGROUND_COLOR)
-            for event in pygame.event.get():
+            events = pygame.event.get()
+            for event in events:
                 # Allows the window to be closed on QUIT ("X" at top right of the window)
                 if event.type == pygame.QUIT:
                     self.running = False
 
             if self.state == "home":
-                self.home_page()
+                self.home_page(events)
             elif self.state == "input":
-                self.input_page()
+                self.input_page(events)
             elif self.state == "algorithm":
                 self.algorithm_page()
 
@@ -76,63 +95,63 @@ class AlgorithmVisualizer:
 
         pygame.quit()
     
-    def home_page(self):
-        pass
-
-    def input_page(self):
-        pass
-
-    def algorithm_page(self):
-        pass
-
-def init() -> None:
-    binary_search_array: BinarySearch = None
-
-    my_time = pygame.time.get_ticks()
-    interval = 500
-    running = True
-
-    display_input = True
-    while running:
-        screen.fill(colors.BACKGROUND_COLOR)
-
-        events = pygame.event.get()
-        if (display_input):
-            input_box.draw(events)
-            val_box.draw(events)
-            submit_btn.draw()
-
-        if binary_search_array:
-            binary_search_array.draw(screen)
-            current_time = pygame.time.get_ticks()
-            if current_time - my_time >= interval:
-                my_time = current_time
-                binary_search_array.draw(screen)
-                binary_search_array.search_step()
+    def home_page(self, events):
+        # Home page contains buttons to select an algorithm
+        for button in self.home_buttons:
+            button[1].draw()
 
         for event in events:
-            # Allows the window to be closed on QUIT ("X" at top right of the window)
-            if event.type == pygame.QUIT:
-                running = False
-            
-            # Handles left mouse press
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_x, mouse_y = pygame.mouse.get_pos()
-                if submit_btn.is_mouse_over(mouse_x, mouse_y):
-                    cell_array, val = submit_btn.on_click()
-                    binary_search_array = BinarySearch(cell_array, val, 50, 140)
-                    display_input = False
+                for button in self.home_buttons:
+                    algorithm, btn = button
+                    if (btn.is_mouse_over(mouse_x, mouse_y)):
+                        # Set the algorithm and change the program state to the algorithm argument input section
+                        self.algorithm_chosen = algorithm
+                        self.state = "input"
+    
+    _ui_variables_bounded = False
+    def input_page(self, events):
+        # Only initialize the ui element variables one time
+        if not self._ui_variables_bounded:
+            self.initialize_ui_elements()
+            self._ui_variables_bounded = True
+            return
 
-                input_box.handle_focus(events, mouse_x, mouse_y)
-                val_box.handle_focus(events, mouse_x, mouse_y)
+        # Render the UI elements
+        for button in self.buttons:
+            button.draw()
 
-        # Displays contents onto screen
-        pygame.display.update()
+        for input_box in self.input_boxes:
+            input_box.draw(events)
 
-        # Sets the FPS of the window to 60
-        clock.tick(60)
+        # UI elements typically have the same functionality, however logic changes on buttons on_click functions
+        for event in events:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                for button in self.buttons:
+                    if button.is_mouse_over(mouse_x, mouse_y):
+                        match self.algorithm_chosen:
+                            case "bsa":
+                                user_input, val = button.on_click()
+                                cell_array = self.cell_array_init(user_input)
+                                self.current_algorithm_obj = BinarySearch(cell_array, int(val), 50, 140)
+                        self.state = "algorithm"
 
-    pygame.quit()
+                for input_box in self.input_boxes:
+                    input_box.handle_focus(events, mouse_x, mouse_y)
+
+    def algorithm_page(self):
+        # Draw out the algorithms
+        if self.algorithm_chosen == "bsa":
+            binary_search_array: BinarySearch = self.current_algorithm_obj
+            if binary_search_array:
+                binary_search_array.draw(screen)
+                current_time = pygame.time.get_ticks()
+                if current_time - self.time >= self.interval:
+                    self.time = current_time
+                    binary_search_array.draw(screen)
+                    binary_search_array.search_step()
 
 
 if __name__ == "__main__":
